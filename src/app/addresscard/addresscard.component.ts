ae7d76157f4e8df5c81e39939a1127f439546d65
Shashank022 <shashank022@gmail.com>
2020-01-15 11:52:09 -0500

    Added changes

---
+++ src/app/addresscard/addresscard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addresscard',
+  templateUrl: './addresscard.component.html',
+  styleUrls: ['./addresscard.component.css']
+})
+export class AddresscardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
